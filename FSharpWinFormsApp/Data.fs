1d9eb895b14843685373d7c03335b7d18ea373e5
sara mahmoud <saraomar1712004@gmail.com>
2024-12-13 21:02:36 +0200

    Version with Database

---
+++ FSharpWinFormsApp/Data.fs
@@ -0,0 +1,13 @@
+﻿module Data
+open MySql.Data.MySqlClient
+
+let connectionString = "Server=localhost;Database=cinema;Uid=root;Pwd=;"
+
+let testconnection () =
+    use conn = new MySqlConnection(connectionString)
+    conn.Open()
+    printfn "connection successful!"
+
+testconnection ()
+
+
